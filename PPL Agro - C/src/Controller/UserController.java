2eb7034c5817cb9752390d366b455be434ff7ab2
blangkonsultan <bagaspbsc@gmail.com>
2018-09-29 22:05:38 +0700

    adding AwalView

---
+++ PPL Agro - C/src/Controller/UserController.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.UserModel;
+import View.AwalView;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author adheraprabu
+ */
+public class UserController {
+
+    UserModel userM = new UserModel();
+    AwalView awal = new AwalView();
+
+    public UserController(AwalView awal, UserModel userM) {
+        //Constructor untuk memanggil frame Awal
+        this.awal = awal;
+        this.userM = userM;
+        awal.setVisible(true);
+        awal.MulaiMouseListener(new MulaiMouseListenner());
+        awal.BantuanMouseListener(new BantuanMouseListener());
+        awal.TentangMouseListener(new TentangMouseListenner());
+        awal.KeluarMouseListener(new KeluarMouseListenner());
+    }
+
+    private void setIcon(JButton button, String resource) {
+        //Method untuk mengganti icon button
+        button.setIcon(new ImageIcon(getClass().getResource(resource)));
+    }
+
+    private class KeluarMouseListenner implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            setIcon(awal.getButton_Keluar(), "/View/AwalUI/KELUAR2.png");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            setIcon(awal.getButton_Keluar(), "/View/AwalUI/KELUAR.png");
+        }
+    }
+
+    private class TentangMouseListenner implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            setIcon(awal.getButton_Tentang(), "/View/AwalUI/TENTANG2.png");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            setIcon(awal.getButton_Tentang(), "/View/AwalUI/TENTANG.png");
+        }
+    }
+
+    private class BantuanMouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            setIcon(awal.getButton_Bantuan(), "/View/AwalUI/BANTUAN2.png");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            setIcon(awal.getButton_Bantuan(), "/View/AwalUI/BANTUAN.png");
+        }
+    }
+
+    private class MulaiMouseListenner implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            setIcon(awal.getButton_Mulai(), "/View/AwalUI/MULAI2.png");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            setIcon(awal.getButton_Mulai(), "/View/AwalUI/MULAI.png");
+        }
+    }
+}
---
+++ PPL Agro - C/src/ppl/agro/c/PPLAgroC.java
@@ -0,0 +1,18 @@
+package ppl.agro.c;
+
+import Controller.UserController;
+import Model.UserModel;
+import View.AwalView;
+import View.PopUpKeluarView;
+
+public class PPLAgroC {
+
+    public static void main(String[] args) {
+//        AwalView awal = new AwalView();
+//        UserModel userM = new UserModel();
+//        new UserController(awal, userM);
+
+
+    }
+
+}
